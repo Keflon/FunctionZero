3af169e77df2eca471191bfd1b7b5b507d9e1c6e
Keith Pickford <Keith@FunctionZero.com>
2024-09-07 15:51:46 +0100

    GridViewZero WIP.

--- ControlsZero/Controls/GridView/GridViewZero.xaml.cs
+++ ControlsZero/Controls/GridView/GridViewZero.xaml.cs
@@ -2,15 +2,24 @@ using FunctionZero.Maui.Controls;
 using System.Collections;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
+using System.Windows.Input;
 
 namespace FunctionZero.Maui.Controls;
 
 public partial class GridViewZero : ContentView
 {
-	public GridViewZero()
+    private bool _pendingUpdateItemContainers = false;
+    private bool _pendingUpdateScrollViewContentHeight = false;
+    private const double MAX_SCROLL_HEIGHT = 2000000.0;
+    private double _scaleToControl = 1.0;
+    private bool _pendingSelectionUpdate = false;
+
+    public GridViewZero()
 	{
 		InitializeComponent();
 	}
+
+    #region bindable properties
 
     #region ColumnsSourceProperty
 
@@ -70,4 +79,326 @@ public partial class GridViewZero : ContentView
     }
 
     #endregion
+
+    #region ItemsSourceProperty
+
+    public static readonly BindableProperty ItemsSourceProperty = BindableProperty.Create(nameof(ItemsSource), typeof(IList), typeof(GridViewZero), null, BindingMode.OneWay, null, ItemsSourceChanged);
+
+    public IList ItemsSource
+    {
+        get { return (IList)GetValue(ItemsSourceProperty); }
+        set { SetValue(ItemsSourceProperty, value); }
+    }
+
+    private static void ItemsSourceChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        var self = (GridViewZero)bindable;
+
+        if (oldValue is INotifyCollectionChanged oldCollection)
+            oldCollection.CollectionChanged -= self.ItemsSource_CollectionChanged;
+
+        if (newValue is INotifyCollectionChanged newCollection)
+            newCollection.CollectionChanged += self.ItemsSource_CollectionChanged;
+
+        self.DeferredUpdateScrollViewContentHeight();
+        self.DeferredUpdateItemContainers();
+    }
+
+    #endregion
+
+    #region RemainingItemsProperty
+
+    public static readonly BindableProperty RemainingItemsProperty = BindableProperty.Create(nameof(RemainingItems), typeof(int), typeof(GridViewZero), -1, BindingMode.OneWay, null, RemainingItemsChanged);
+
+    public int RemainingItems
+    {
+        get { return (int)GetValue(RemainingItemsProperty); }
+        set { SetValue(RemainingItemsProperty, value); }
+    }
+
+    private static void RemainingItemsChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        var self = (GridViewZero)bindable;
+
+        self.RemainingItemsChangedCommand?.Execute(newValue);
+    }
+
+    #endregion
+
+    #region RemainingItemsChangedCommand
+
+    public static readonly BindableProperty RemainingItemsChangedCommandProperty = BindableProperty.Create(nameof(RemainingItemsChangedCommand), typeof(ICommand), typeof(GridViewZero), null, BindingMode.OneWay, null, RemainingItemsChangedCommandChanged);
+
+    public ICommand RemainingItemsChangedCommand
+    {
+        get { return (ICommand)GetValue(RemainingItemsChangedCommandProperty); }
+        set { SetValue(RemainingItemsChangedCommandProperty, value); }
+    }
+
+    private static void RemainingItemsChangedCommandChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        var self = (GridViewZero)bindable;
+    }
+
+    #endregion
+
+    #region SelectedItemsProperty
+
+    public static readonly BindableProperty SelectedItemsProperty = BindableProperty.Create(nameof(SelectedItems), typeof(IList), typeof(GridViewZero), null, BindingMode.TwoWay, null, SelectedItemsChanged);
+
+    private static void SelectedItemsChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        var self = (GridViewZero)bindable;
+
+        if (oldValue is INotifyCollectionChanged oldCollection)
+            oldCollection.CollectionChanged -= self.SelectedItems_CollectionChanged;
+
+        if (newValue is INotifyCollectionChanged newCollection)
+            newCollection.CollectionChanged += self.SelectedItems_CollectionChanged;
+
+        // This will bail early if the change was caused by SelectionUpdate.
+        self.DeferredSelectionUpdate();
+    }
+    public IList SelectedItems
+    {
+        get { return (IList)GetValue(SelectedItemsProperty); }
+        set { SetValue(SelectedItemsProperty, value); }
+    }
+
+    #endregion
+
+    #region SelectionModeProperty
+
+    public static readonly BindableProperty SelectionModeProperty = BindableProperty.Create(nameof(SelectionMode), typeof(SelectionMode), typeof(GridViewZero), SelectionMode.None, BindingMode.OneWay, null, SelectionModeChanged);
+
+    public SelectionMode SelectionMode
+    {
+        get { return (SelectionMode)GetValue(SelectionModeProperty); }
+        set { SetValue(SelectionModeProperty, value); }
+    }
+
+    private static void SelectionModeChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        var self = (GridViewZero)bindable;
+
+        self.DeferredSelectionUpdate();
+    }
+
+    #endregion
+
+    #region SelectedItemProperty
+
+    public static readonly BindableProperty SelectedItemProperty = BindableProperty.Create(nameof(SelectedItem), typeof(object), typeof(GridViewZero), null, BindingMode.TwoWay, null, SelectedItemChanged);
+
+    private static void SelectedItemChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        var self = (GridViewZero)bindable;
+
+        if (self.SelectionMode == SelectionMode.None)
+        {
+            self.SelectedItem = null;
+        }
+        else
+        {
+            var newContainer = self.GetViewForBindingContextFromCanvas(newValue);
+
+            if (newContainer is ListItemZero listItem)
+                listItem.IsSelected = true;
+        }
+    }
+    public object SelectedItem
+    {
+        get { return (object)GetValue(SelectedItemProperty); }
+        set { SetValue(SelectedItemProperty, value); }
+    }
+
+    #endregion
+
+    #region ScrollOffsetProperty
+
+    public static readonly BindableProperty ScrollOffsetProperty = BindableProperty.Create(nameof(ScrollOffset), typeof(double), typeof(GridViewZero), (double)0.0, BindingMode.TwoWay, null, ScrollOffsetChanged, null);
+
+    private static void ScrollOffsetChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        var self = (GridViewZero)bindable;
+
+        // TODO: Lookahead and Lookbehind values; to prevent flicker on Windows when fast scolling.
+
+        // Windows is less flickery if we defer the update here. That doesn't make sense!
+        // iOS and Droid don't flicker and look the same either way. That doesn't make sense either!
+        self.DeferredUpdateItemContainers();
+
+        // If the scroll movement came from the ScrollView don't write it back, because doing so upsets iOS when scrolling outside of bounds and is unnecessary anyway.
+
+        double actualScrollOffset = self.theScrollView.ScrollY / self._scaleToControl;
+
+        if (self.ScrollOffset != actualScrollOffset)
+            //self.DeferredScrollTo(self.ScrollOffset * self._scaleToControl);
+            self.theScrollView.ScrollYRequest = self.ScrollOffset * self._scaleToControl;
+    }
+    public double ScrollOffset
+    {
+        get { return (double)GetValue(ScrollOffsetProperty); }
+        set { SetValue(ScrollOffsetProperty, value); }
+    }
+
+    #endregion
+
+    #region ItemHeightProperty
+
+    public static readonly BindableProperty ItemHeightProperty = BindableProperty.Create(nameof(ItemHeight), typeof(double), typeof(GridViewZero), (double)40.0, BindingMode.OneWay);
+
+    public double ItemHeight
+    {
+        get { return (double)GetValue(ItemHeightProperty); }
+        set { SetValue(ItemHeightProperty, value); }
+    }
+
+    #endregion
+
+    #region ItemContainerStyleProperty
+
+    public static readonly BindableProperty ItemContainerStyleProperty = BindableProperty.Create(nameof(ItemContainerStyle), typeof(Style), typeof(GridViewZero), null, BindingMode.OneWay, null, ItemContainerStyleChanged);
+
+    public Style ItemContainerStyle
+    {
+        get { return (Style)GetValue(ItemContainerStyleProperty); }
+        set { SetValue(ItemContainerStyleProperty, value); }
+    }
+
+    private static void ItemContainerStyleChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        var self = (GridViewZero)bindable;
+    }
+
+    #endregion
+
+    #endregion
+
+
+    private void SelectedItems_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+    {
+        // This will bail early if the change was caused by DeferredFilter.
+        DeferredSelectionUpdate();
+    }
+
+    private void DeferredSelectionUpdate()
+    {
+        if (_pendingSelectionUpdate == false)
+        {
+            _pendingSelectionUpdate = true;
+            // The underlying collection can have items added / removed in a foreach,
+            // and this buffers that down to 1 operation.
+            Dispatcher.Dispatch(() =>
+            {
+                switch (SelectionMode)
+                {
+                    case SelectionMode.None:
+                        SelectedItems.Clear();
+                        break;
+                    case SelectionMode.Single:
+                        if (SelectedItems.Count > 1)
+                        {
+                            // TODO: KillList, to reduce callbacks if SelectedItems is observable.
+                            var temp = SelectedItems[SelectedItems.Count - 1];
+                            SelectedItems.Clear();
+                            SelectedItems.Add(temp);
+                        }
+                        break;
+                    case SelectionMode.Multiple:
+                        break;
+                }
+
+                if (SelectedItems.Contains(SelectedItem) == false)
+                {
+                    if (SelectedItems.Count > 0)
+                        SelectedItem = SelectedItems[SelectedItems.Count - 1];
+                    else
+                        SelectedItem = null;
+                }
+
+                foreach (View item in this.theScrollView.Canvas)
+                    if (item is ListItemZero listItem)
+                    {
+                        listItem.IsSelected = SelectedItems.Contains(listItem.BindingContext);
+                        listItem.IsPrimary = listItem.BindingContext == SelectedItem;
+                    }
+
+                _pendingSelectionUpdate = false;
+            }
+            );
+        }
+    }
+
+    private void ItemsSource_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+    {
+        DeferredUpdateScrollViewContentHeight();
+        DeferredUpdateItemContainers();
+    }
+
+    private void DeferredUpdateScrollViewContentHeight()
+    {
+        if (_pendingUpdateScrollViewContentHeight == false)
+        {
+            _pendingUpdateScrollViewContentHeight = true;
+
+            Dispatcher.Dispatch(() =>
+            {
+                UpdateScrollViewContentHeight();
+
+#if false
+// TODO: If anyone needs it, cap the HeightRequest to something manageable and scale the rendering offsets appropriately.
+
+                //canvas.HeightRequest = 2090000;
+                //canvas.HeightRequest = 2100000;
+                //canvas.HeightRequest = 2097590;clip region slightly too big
+                //canvas.HeightRequest = 2098600; //clip region massive/ not working 
+                //canvas.HeightRequest = 2097589; 
+#endif
+                _pendingUpdateScrollViewContentHeight = false;
+            }
+            );
+        }
+    }
+
+    private void UpdateScrollViewContentHeight()
+    {
+        double desiredHeight = ItemHeight * ItemsSource?.Count ?? 0;
+
+
+        if (desiredHeight > MAX_SCROLL_HEIGHT)
+        {
+            _scaleToControl = MAX_SCROLL_HEIGHT / desiredHeight;
+            theScrollView.ContentHeight = MAX_SCROLL_HEIGHT + (this.Height - this.Height * (_scaleToControl));
+        }
+        else
+        {
+            theScrollView.ContentHeight = desiredHeight;
+            _scaleToControl = 1.0;
+        }
+    }
+
+    private void DeferredUpdateItemContainers()
+    {
+        if (_pendingUpdateItemContainers == false)
+        {
+            _pendingUpdateItemContainers = true;
+            // The underlying collection can have items added / removed in a foreach,
+            // and this buffers that down to 1 call to UpdateItemContainers.
+            Dispatcher.Dispatch(() =>
+            {
+                UpdateItemContainers();
+                _pendingUpdateItemContainers = false;
+            }
+            );
+        }
+    }
+
+    private void UpdateItemContainers()
+    {
+        foreach (var item in ColumnsSource)
+        {
+            item.UpdateItemContainers(this);
+        }
+    }
 }
