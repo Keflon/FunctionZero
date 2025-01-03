bf2c34982a42e8b26691aae7a7e291eafadeafcb
Keith Pickford <Keith@FunctionZero.com>
2025-01-03 10:19:59 +0000

    Added CollectionsZero with ObsCollZero<T>

---
+++ CollectionsZero/ObsCollZero.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Data;
+using System.Net;
+
+namespace CollectionsZero
+{
+    public class ObsCollZero<T> : ObservableCollection<T>
+    {
+        private readonly Action<T> _attachAction;
+        private readonly Action<T> _detachAction;
+
+        public ObsCollZero(Action<T> attachAction, Action<T> detachAction) : base()
+        {
+            _attachAction = attachAction ?? ((T t) => { });
+            _detachAction = detachAction ?? ((T t) => { });
+        }
+        public ObsCollZero(Action<T> attachAction, Action<T> detachAction, IEnumerable<T> values) : base(values)
+        {
+            _attachAction = attachAction ?? ((T t) => { });
+            _detachAction = detachAction ?? ((T t) => { });
+        }
+        public ObsCollZero(Action<T> attachAction, Action<T> detachAction, List<T> values) : base(values)
+        {
+            _attachAction = attachAction ?? ((T t) => { });
+            _detachAction = detachAction ?? ((T t) => { });
+        }
+
+
+        protected override void InsertItem(int index, T item)
+        {
+            base.InsertItem(index, item);
+            _attachAction(item);
+        }
+
+        protected override void RemoveItem(int index)
+        {
+            _detachAction(this[index]);
+            base.RemoveItem(index);
+        }
+
+        protected override void ClearItems()
+        {
+            while (this.Count != 0)
+                RemoveItem(this.Count - 1);
+        }
+    }
+}
---
+++ TestProject/CollectionZeroTests.cs
@@ -0,0 +1,39 @@
+﻿using CollectionsZero;
+using System.Diagnostics;
+
+namespace TestProject
+{
+    [TestClass]
+    public sealed class CollectionZeroTests
+    {
+        [TestMethod]
+        public void TestInsert()
+        {
+            var collection = new ObsCollZero<int>(AttachItem, DetachItem);
+
+            collection.Add(4);
+
+            collection.Add(6);
+
+            collection.Add(2);
+            collection.Add(5);
+
+            collection.RemoveAt(1);
+
+            collection.Remove(2);
+
+            collection.Clear();
+
+        }
+        private void AttachItem(int obj)
+        {
+            Debug.WriteLine($"Attached to {obj}");
+        }
+
+        private void DetachItem(int obj)
+        {
+            Debug.WriteLine($"Detached from {obj}");
+
+        }
+    }
+}
